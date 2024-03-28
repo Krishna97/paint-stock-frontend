ad672f09692ef2602c3fe946fba5772f5e60a788
Krishna Sudani <krishnasudani97@gmail.com>
2024-03-28 06:47:34 -0700

    add api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'viaduct.proxy.rlwy.net:35134'
+  apiUrl: 'https://backend-onlymyslef.koyeb.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3100'
+  apiUrl: 'https://backend-onlymyslef.koyeb.app'
 };
 
 /*
